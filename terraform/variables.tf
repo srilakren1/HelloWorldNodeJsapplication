708fcac7b9970d977960958651900b68e9fb352d
srilakren1 <srilakren@gmail.com>
2025-09-19 20:11:20 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,15 @@
-variable "environment" {
-  description = "Deployment environment (dev/stage/prod)"
+variable "project_id" {
+  description = "The ID of the GCP project"
+  type        = string
+}
+
+variable "region" {
+  description = "The region to create the bucket in"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "bucket_name" {
+  description = "The name of the GCS bucket"
   type        = string
 }
